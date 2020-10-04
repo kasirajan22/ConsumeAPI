a40c1ff96e4a5e817b64cbe91d3169edb2a79f7a
kasirajan22 <kasi.srm@gmail.com>
2020-10-04 19:05:25 +0530

    Initial Commit

---
+++ ConsumeAPI/ConsumeAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConsumeAPIApp.swift
+//  ConsumeAPI
+//
+//  Created by Magizhan on 04/10/20.
+//
+
+import SwiftUI
+
+@main
+struct ConsumeAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
